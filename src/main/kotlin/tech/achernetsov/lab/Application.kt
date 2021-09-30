739ef65c23543d6c27d2aca88913018a33c37362
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2021-10-01 02:17:20 +0700

    running app with HOCON config

--- src/main/kotlin/tech/achernetsov/lab/Application.kt
+++ src/main/kotlin/tech/achernetsov/lab/Application.kt
@@ -1,15 +1,14 @@
 package tech.achernetsov.lab
 
+import io.ktor.application.*
 import tech.achernetsov.lab.plugins.configureDependencyInjection
 import tech.achernetsov.lab.plugins.configureRoutingAndViews
 import tech.achernetsov.lab.plugins.configureSecurity
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "localhost") {
-        configureDependencyInjection()
-        configureSecurity()
-        configureRoutingAndViews()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    configureDependencyInjection()
+    configureSecurity()
+    configureRoutingAndViews()
 }
